b057ce4d8d82c197c378242a4c097c1ad0a1da36
Den4i <dglonassik@gmail.com>
2018-02-21 21:31:39 +0300

    Add Links to menu

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Navbar from 'react-bootstrap/lib/Navbar';
+import {Link} from 'react-router-dom';
 
-const menu = ["Правила обмена", "Партнерам", "Новости", "Контакты", "FAQ"];
+const menu = [
+        {id:1, name: "Главная", path: "/"},
+        {id:2, name: "Правила обмена", path: "/rules"},
+        {id:3, name:"Партнерам", path: "/partners"},
+        {id:4, name:"Новости", path: "/news"},
+        {id:5, name:"Контакты", path: "/contacts"},
+        {id:6, name:"FAQ", path: "/faq"}
+    ];
 
-function Navigation() {
-    return (
-        <div>
-            <Navbar>
-                <Navbar.Header>
-                    {menu.map((nav) =>
-                        <Navbar.Brand key={nav}>
-                            <span>{nav}</span>
-                        </Navbar.Brand>)}
-                </Navbar.Header>
-            </Navbar>
-        </div>
-    )
+class Navigation extends Component {
+    render (){
+        return (
+            <div>
+                <Navbar>
+                    <Navbar.Header>
+                         {
+                             menu.map((nav) =>
+                                <Navbar.Brand key={nav.id}>
+                                    <Link to={nav.path}>{nav.name}</Link>
+                                </Navbar.Brand>
+                             )
+                         }
+                    </Navbar.Header>
+                </Navbar>
+
+            </div>
+        )
+    }
 }
 
 export default Navigation;
