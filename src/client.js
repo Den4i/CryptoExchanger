ce54392f400625a66a4f0f6bb0f1c8edad59bb39
Den4i <dglonassik@gmail.com>
2018-02-21 23:34:09 +0300

    Add NotFound Component

--- src/client.js
+++ src/client.js
@@ -8,16 +8,20 @@ import './styles.css'
 
 import ExchangeRules from './components/ExchangeRules/ExchangeRules';
 import Navigation from './components/Navigation/Navigation';
+import NotFound from './components/NotFound/NotFound';
 
 ReactDOM.render(
-        <div>
-            <Navigation />
-            <BrowserRouter>
+    <div>
+        <BrowserRouter>
+            <div>
+                <Route component={Navigation} />
                 <Switch>
                     <Route exact path={'/'} component={App} />
                     <Route path={'/rules'} component={ExchangeRules} />
+                    <Route component={NotFound} />
                 </Switch>
-            </BrowserRouter>
-        </div>,
-        document.getElementById("poloniex")
+            </div>
+        </BrowserRouter>
+    </div>,
+    document.getElementById("poloniex")
 );
