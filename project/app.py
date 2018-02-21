624e2886a98717e5a39d1605e4fdca995e367de8
Den4i <dglonassik@gmail.com>
2018-02-21 21:30:44 +0300

    Add page 404 processing

--- project/app.py
+++ project/app.py
@@ -25,5 +25,10 @@ def poloniex():
     return jsonify(ticker)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return send_from_directory(assetsPath, "index.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
