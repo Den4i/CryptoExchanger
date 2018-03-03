b114f65784f9517b7cea0926a70c8b6092b09778
Den4i <dglonassik@gmail.com>
2018-03-04 00:15:28 +0300

    Adding new component to BrowserRouter

--- src/client.js
+++ src/client.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import './bootstrap.css';
 import './styles.css'
 
+import App from './components/App/App';
 import ExchangeRules from './components/ExchangeRules/ExchangeRules';
 import Navigation from './components/Navigation/Navigation';
 import NotFound from './components/NotFound/NotFound';
+import Partners from './components/Partners/Partners';
+import News from './components/News/News';
+import Contacts from './components/Contacts/Contacts';
+import FAQ from './components/FAQ/FAQ';
 
 ReactDOM.render(
     <div>
@@ -18,6 +22,10 @@ ReactDOM.render(
                 <Switch>
                     <Route exact path={'/'} component={App} />
                     <Route path={'/rules'} component={ExchangeRules} />
+                    <Route path={'/partners'} component={Partners} />
+                    <Route path={'/news'} component={News} />
+                    <Route path={'/contacts'} component={Contacts} />
+                    <Route path={'/faq'} component={FAQ} />
                     <Route component={NotFound} />
                 </Switch>
             </div>
