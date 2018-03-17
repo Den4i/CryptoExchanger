e6867082d225d9bcbf06812f1a4ed4f623dc30fa
Den4i <dglonassik@gmail.com>
2018-03-18 00:25:14 +0300

    Adding new test for App component

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,15 +1,32 @@
-import { renderComponent , expect } from '../test_helper';
-import App from '../../src/components/App/App';
+import { React, expect, shallow } from '../test_helper';
+import { App, LeftInput, ResultInput, PoloniexTicket, Course } from '../../src/components/App/App';
 
 
 describe('App' , () => {
   let component;
 
   beforeEach(() => {
-    component = renderComponent(App);
+    component = shallow(<App />);
   });
 
-  it('render Application', () => {
+  it('renders Application', () => {
     expect(component).to.exist;
   });
+
+  it('has a LeftInput', () => {
+    expect(component.find(LeftInput)).to.have.length(1);
+  });
+
+  it('has ResultInput', () => {
+    expect(component.find(ResultInput)).to.have.length(1);
+  });
+
+  it('has two PoloniexTicket', () => {
+    expect(component.find(PoloniexTicket)).to.have.length(2);
+  });
+
+   it('has Course', () => {
+    expect(component.find(Course)).to.have.length(1);
+  });
+
 });
