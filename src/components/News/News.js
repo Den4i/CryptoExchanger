9cd343e575f6d39e77195f7f6c2d41ef742a5323
Den4i <dglonassik@gmail.com>
2018-03-28 23:05:40 +0300

    Adding News and NewsList component

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,9 +1,41 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { news } from '../../tmpDefines'
 
-export default class News extends Component {
+class NewsList extends Component {
     render(){
+        let object_list = news.map((item) => (
+            <div className={"jumbotron"} key={ item.id }>
+              <h1 className={"display-5"}> { item.title } </h1>
+              <p className={"lead"}> { item.short } </p>
+              <p className={"lead"}>
+                <a className={"btn btn-primary btn-lg"} href={"/news/"+item.id} role="button">Читать подробнее</a>
+              </p>
+            </div>
+            )
+        );
+
         return(
-            <div>Новости</div>
+            <div>
+                { object_list }
+            </div>
         )
     }
 }
+
+class News extends Component{
+    render(){
+        const NewsId = this.props.match.params.id;
+        return <h2>Товар № { NewsId }</h2>;
+    }
+}
+
+News.propTypes = {
+    match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired
+    })
+  })
+};
+
+export { NewsList, News }
