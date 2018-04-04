bc8e5ae8a5fa8499458583bd7922b3081d1a5052
Den4i <dglonassik@gmail.com>
2018-04-05 00:24:30 +0300

    Adding path to system path

--- project/app.py
+++ project/app.py
@@ -1,5 +1,12 @@
 from flask import Flask, send_from_directory, jsonify
 from os import path
+
+import sys
+path1 = path.dirname(path.dirname(path.abspath(__file__)))
+
+if path1 not in sys.path:
+    sys.path.append(path1)
+
 from project.poloniex_api import Poloniex
 from flask_cors import cross_origin
 
