5d8c30b7450f637b4ef039013d8b5fbbe5377720
Den4i <dglonassik@gmail.com>
2018-04-05 00:25:42 +0300

    Adding Question component

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,9 +1,38 @@
-import React, {Component} from 'react';
+import React from 'react';
+import PropTypes from "prop-types";
+import { faqList } from '../../tmpDefines';
 
-export default class FAQ extends Component{
-    render(){
-        return (
-            <div>FAQ</div>
+const FAQ = () => {
+    let faqList1 = faqList.map((item) => (
+		<div key={item.id}>
+			<a className={"btn btn-primary btn-md"} href={"/faq/"+item.id} role="button" >{ item.title}</a>
+		</div>
         )
+    );
+    return (
+        <div>
+			{ faqList1 }
+        </div>
+    )
+};
+
+const Question = (props) => {
+    const faqId = props.match.params.id;
+    let content;
+    for (let i of faqList) {
+        if (""+i.id === faqId){
+            content = i.content;
+        }
     }
-}
+    return <div>{ content }</div>;
+};
+
+Question.propTypes = {
+    match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired
+    })
+  })
+};
+
+export { FAQ, Question };
