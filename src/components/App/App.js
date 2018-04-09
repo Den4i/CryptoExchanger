f7e9617cd2da2ea7bb02c8f6c7c9bb47ecacc1af
Den4i <dglonassik@gmail.com>
2018-04-10 00:58:50 +0300

    Replace Course to DataEntry component, adding data state and fetchData

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,83 +1,95 @@
+/* eslint-disable max-len,require-jsdoc,no-invalid-this */
 import React from 'react';
 import PoloniexTicket from '../PoloniexTicket/PoloniexTicket';
-import Course from '../Course/Course';
 import PropTypes from 'prop-types';
+import DataEntry from '../DataEntry/DataEntry';
 
 
 class App extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {currentLeft: "", currentRight: "", result: ""};
+        this.state = {currentLeft: '', currentRight: '', result: '', data: {}};
     }
 
-  getCurrencyLeft = (e) => {
-      this.setState({currentLeft: e});
-  };
+    getCurrencyLeft = (e) => {
+        this.setState({currentLeft: e});
+    };
 
 
-  getCurrencyRight = (e) => {
-      this.setState({currentRight: e});
-  };
+    getCurrencyRight = (e) => {
+        this.setState({currentRight: e});
+    };
 
-  getResult = () => {
-      let left = this.state.currentLeft;
-      let right = this.state.currentRight;
-      let lastCurrency = "";
-      if (this.state.currentLeft !== "" && this.state.currentRight !== ""){
-        let polo = sessionStorage.getItem("poloniex");
-        let data = JSON.parse(polo);
+    getResult = () => {
+        let left = this.state.currentLeft;
+        let right = this.state.currentRight;
+        let lastCurrency = '';
+        if (this.state.currentLeft !== '' && this.state.currentRight !== '') {
+            const data = this.state.data;
 
-        if (data[left+"_"+right]){
-            lastCurrency = 1 / data[left+"_"+right].last;
-        } else
-        if (data[right+"_"+left]){
-            lastCurrency = data[right+"_"+left].last
-        } else {
-            lastCurrency = 'Нет курса обмена';
+            if (!data[left + '_' + right]) {
+                if (data[right + '_' + left]) {
+                    lastCurrency = data[right + '_' + left].last;
+                } else {
+                    lastCurrency = 'Нет курса обмена';
+                }
+            } else {
+                lastCurrency = 1 / data[left + '_' + right].last;
+            }
         }
-      }
 
-      return lastCurrency.toString();
-  };
+        return lastCurrency.toString();
+    };
 
-  leftInLog = (e) => {
-      let result = parseInt(e.target.value)* this.getResult();
-      this.setState({result: ""+result});
-  };
+    leftInLog = (e) => {
+        let result = parseInt(e.target.value)* this.getResult();
+        this.setState({result: ''+result});
+    };
 
-  render(){
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    async fetchData() {
+        const response = await fetch('http://127.0.0.1:5000/poloniex');
+        const data = await response.json();
+        this.setState({data: data});
+    }
+
+    render() {
         return (
-            <div className={"col-12"}>
-                <div style={{float:"left"}}>
+            <div className={'calc'}>
+                <div className={'calc_columns'}>
                     <LeftInput name={this.state.currentLeft} onCount={this.leftInLog} />
-                    <PoloniexTicket say = {this.getCurrencyLeft}/>
+                    <PoloniexTicket say={this.getCurrencyLeft}/>
                 </div>
-                <div style={{float:"left"}}>
+                <div className={'calc_columns'}>
                     <ResultInput result={this.state.result} />
-                    <PoloniexTicket say = {this.getCurrencyRight} result={this.state.result}/>
+                    <PoloniexTicket say={this.getCurrencyRight}/>
                 </div>
-                <div style={{float:"left"}}><Course lastCurrency = {this.getResult()}/></div>
+                <div className={'calc_columns'}><DataEntry lastCurrency = {this.getResult()} /></div>
             </div>
-        )
+        );
     }
 }
 
-const LeftInput = function (props) {
-    return <input type="number" name={props.currentLeft} onChange={props.onCount} value={props.result} className={'form-control'}/>
+const LeftInput = function(props) {
+    return <input type="number" name={props.currentLeft} onChange={props.onCount} value={props.result} min={'1'}
+                  className={'form-control'}/>;
 };
 
-const ResultInput = function (props) {
-    return <input type="text" value={props.result} className={'form-control'}/>
+const ResultInput = function(props) {
+    return <input type="text" value={props.result} className={'form-control'}/>;
 };
 
 LeftInput.propTypes = {
-  currentLeft: PropTypes.string,
-  onCount: PropTypes.func,
-  result: PropTypes.string
+    currentLeft: PropTypes.string,
+    onCount: PropTypes.func,
+    result: PropTypes.string,
 };
 
 ResultInput.propTypes = {
-  result: PropTypes.string
+    result: PropTypes.string,
 };
 
-export { App, LeftInput, ResultInput, PoloniexTicket, Course };
+export {App};
