729cbd0db9e2e8406ceac2eab3ce861b00c7e0ce
Den4i <dglonassik@gmail.com>
2018-04-10 00:28:23 +0300

    Created a component CryptList derived from PoloniexTicket

---
+++ src/components/CryptList/CryptList.js
@@ -0,0 +1,33 @@
+/* eslint-disable max-len,require-jsdoc */
+import React from 'react';
+import PropTypes from 'prop-types';
+import {cryptListArray} from '../../tmpDefines';
+
+
+class CryptList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(e) {
+        this.props.handleChange(e.target.name);
+    }
+
+    render() {
+        const buttonList = cryptListArray.map((item) => (<button key={item} name={item} onClick={this.handleChange}
+                    className ={this.props.current === item ? 'btn btn-success': 'btn btn-secondary'}>{item}</button>));
+        return (
+            <div className="btn-group-vertical" role="group">
+                {buttonList}
+            </div>
+        );
+    }
+}
+
+CryptList.propTypes = {
+    handleChange: PropTypes.func,
+    current: PropTypes.string,
+};
+
+
+export default CryptList;
