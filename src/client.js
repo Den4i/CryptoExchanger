0f2db71cf9ea2ae164a9393d22d6f8bd1d49a120
Den4i <dglonassik@gmail.com>
2018-04-26 00:43:04 +0300

    Create store with thunk

--- src/client.js
+++ src/client.js
@@ -4,13 +4,30 @@ import {BrowserRouter} from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
 
 import './bootstrap.css';
-import './styles.css'
+import './styles.css';
+
+import App from '../src/components/App/App';
+import {Provider} from 'react-redux';
+
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import reduce from './store/reducer';
+const store = createStore(reduce, applyMiddleware(thunk));
+
+
+// ReactDOM.render(
+//     <div>
+//         <BrowserRouter>
+//             <Navigation />
+//         </BrowserRouter>
+//     </div>,
+//     document.getElementById('root')
+// );
+
 
 ReactDOM.render(
-    <div>
-        <BrowserRouter>
-            <Navigation />
-        </BrowserRouter>
-    </div>,
-    document.getElementById("root")
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
