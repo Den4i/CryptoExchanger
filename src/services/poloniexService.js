65ff1494427811765739aca222e2b47caf09692a
Den4i <dglonassik@gmail.com>
2018-04-30 23:09:17 +0300

    small Ref

--- src/services/poloniexService.js
+++ src/services/poloniexService.js
@@ -1,6 +1,6 @@
 /* eslint-disable require-jsdoc */
 class PoloniexService {
-    static async fetchData() {
+    async fetchData() {
         const response = await fetch('http://127.0.0.1:5000/poloniex');
         let data = await response.json();
         return data;
