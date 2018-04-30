2f2a6dbe18c732bc0850b5c4dc7b49b7ece38939
Den4i <dglonassik@gmail.com>
2018-04-30 23:06:55 +0300

    Modified Redux and navigation merge

--- src/client.js
+++ src/client.js
@@ -1,33 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import reduce from './store/reducer';
 
 import './bootstrap.css';
 import './styles.css';
 
-import App from '../src/components/App/App';
-import {Provider} from 'react-redux';
-
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import reduce from './store/reducer';
 const store = createStore(reduce, applyMiddleware(thunk));
-
-
-// ReactDOM.render(
-//     <div>
-//         <BrowserRouter>
-//             <Navigation />
-//         </BrowserRouter>
-//     </div>,
-//     document.getElementById('root')
-// );
 
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Navigation />
     </Provider>,
     document.getElementById('root')
 );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,16 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Navbar from 'react-bootstrap/lib/Navbar';
-import { Link, Route, Switch } from 'react-router-dom';
+import {Link, Route, Switch, BrowserRouter} from 'react-router-dom';
 
-import { App } from '../App/App';
+import App from '../App/App';
 import ExchangeRules from '../ExchangeRules/ExchangeRules';
 import NotFound from '../NotFound/NotFound';
 import Partners from '../Partners/Partners';
-import { NewsList, News } from '../News/News';
+import {NewsList, News} from '../News/News';
 import Contacts from '../Contacts/Contacts';
-import { FAQ, Question } from '../FAQ/FAQ';
+import {FAQ, Question} from '../FAQ/FAQ';
 import Registration from '../Registration/Registration';
-import { menu } from '../../tmpDefines'
+import {menu} from '../../tmpDefines';
 
 
 const Nav = () => {
@@ -27,28 +27,30 @@ const Nav = () => {
                 </Navbar.Header>
             </Navbar>
         </div>
-    )
+    );
 };
 
 
 const Navigation = () => {
     return (
-        <div>
-            <Nav/>
-            <Switch>
-                <Route exact path={'/'} component={App}/>
-                <Route path={'/rules'} component={ExchangeRules}/>
-                <Route path={'/partners'} component={Partners}/>
-                <Route exact path={'/news'} component={NewsList}/>
-                <Route path={'/news/:id'} component={News}/>
-                <Route path={'/contacts'} component={Contacts}/>
-                <Route exact path={'/faq'} component={FAQ}/>
-                <Route path={'/faq/:id'} component={Question}/>
-                <Route path={'/registration'} component={Registration}/>
-                <Route component={NotFound}/>
-            </Switch>
-        </div>
-    )
+        <BrowserRouter>
+            <div>
+                <Nav/>
+                <Switch>
+                    <Route exact path={'/'} component={App}/>
+                    <Route path={'/rules'} component={ExchangeRules}/>
+                    <Route path={'/partners'} component={Partners}/>
+                    <Route exact path={'/news'} component={NewsList}/>
+                    <Route path={'/news/:id'} component={News}/>
+                    <Route path={'/contacts'} component={Contacts}/>
+                    <Route exact path={'/faq'} component={FAQ}/>
+                    <Route path={'/faq/:id'} component={Question}/>
+                    <Route path={'/registration'} component={Registration}/>
+                    <Route component={NotFound}/>
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
 };
 
 export default Navigation;
